b8764c17a14a0696f80f86cf5f7612d172dce0d3
swiftFox <rcwilson995@gmail.com>
2018-07-07 17:37:08 +0100

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -15,9 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'John',
+        text: 'see you later',
+        createdAt: 123456
+    });
+
+    socket.on('createMessage', (message) => {
+       console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
+
+
 });
 
 server.listen(port, () => {
