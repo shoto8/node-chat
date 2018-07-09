9ac4606db40d0631c81fa3350efe57ab90bbaaf2
swiftFox <rcwilson995@gmail.com>
2018-07-09 20:01:34 +0100

    updated file for multi-user test

--- public/js/index.js
+++ public/js/index.js
@@ -21,10 +21,12 @@ socket.on('newMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('')
     });
 });
