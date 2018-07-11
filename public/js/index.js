ca572b6303de2453b7e8633f108520511c6b3b28
swiftFox <rcwilson995@gmail.com>
2018-07-11 23:33:08 +0100

    Add scrolling functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
 var socket = io();
 
+//Scrolling decisions
+
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -19,6 +39,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
     // console.log('newMessage', message);
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
