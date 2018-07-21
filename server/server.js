a92a2bd53b56d8230dcd85c272ff0aef09064bd9
swiftFox <rcwilson995@gmail.com>
2018-07-21 17:35:57 +0100

    Add user identification

--- server/server.js
+++ server/server.js
@@ -36,14 +36,13 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-       console.log('createMessage', message);
-       io.emit('newMessage', generateMessage(message.from, message.text));
+        var user = users.getUser(socket.id);
+
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
+
        callback();
-      //   socket.broadcast.emit('newMessage', {
-      //       from: message.from,
-      //       text: message.text,
-      //       createdAt: new Date().getTime()
-      // });
     });
 
 
